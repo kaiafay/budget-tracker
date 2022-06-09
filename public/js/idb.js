3fde8de3aeb11d7cbd46198dfd0013d00cc78905
Kaia Scheirman <kfscheirman18@gmail.com>
2022-06-08 21:18:12 -0700

    fix syntax error

--- public/js/idb.js
+++ public/js/idb.js
@@ -4,7 +4,7 @@ const request = indexedDB.open('budget_tracker', 1);
 // trigger if database version changes
 request.onupgradeneeded = function(e) {
     // save reference to database
-    const db = event.target.result;
+    const db = e.target.result;
     // create object store
     db.createObjectStore('new_transaction', { autoIncrement: true });
 };
